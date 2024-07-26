e340368246764d0f58e7e7efac0a6bb46d1c074d
domashnii22 <domashnii.al@yandex.ru>
2024-07-26 16:15:09 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import './App.css';
+import JournalItem from './components/JournalItem/JournalItem';
+import CardButton from './components/CardButton/CardButton';
+import LeftPanel from './layouts/LeftPanel/LeftPanel';
+import Body from './layouts/Body/Body';
+import Header from './components/Header/Header';
+import JournalList from './components/JournalList/JournalList';
+import JournalAddButton from './components/JournalAddButton/JournalAddButton';
+
+function App() {
+	const data = [
+		{
+			title: 'Подготовка', 
+			text: 'Горные походы',
+			date: new Date()
+		},
+		{
+			title: 'Подготовка2', 
+			text: 'Горные походы2',
+			date: new Date()
+		}
+	];
+
+	return (
+		<div className='app'>
+			<LeftPanel>
+				<Header></Header>
+				<JournalAddButton></JournalAddButton>
+				<JournalList>
+					<CardButton>
+						<JournalItem
+							title={data[0].title}
+							text={data[0].text}
+							date={data[0].date}
+						></JournalItem>
+					</CardButton>
+					<CardButton>
+						<JournalItem
+							title={data[1].title}
+							text={data[1].text}
+							date={data[1].date}
+						></JournalItem>
+					</CardButton>
+				</JournalList>
+			</LeftPanel>
+			<Body>
+    Body
+			</Body>
+		</div>
+	);
+}
+
+export default App;
