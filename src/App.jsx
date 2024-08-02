a8a7b0e0c646ab03d1f534512bccea2b81740462
domashnii22 <domashnii.al@yandex.ru>
2024-08-02 08:51:52 +0300

    add style

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,54 @@ import Header from './components/Header/Header';
 import JournalList from './components/JournalList/JournalList';
 import JournalAddButton from './components/JournalAddButton/JournalAddButton';
 import JournalForm from './components/JournalForm/JournalForm';
+import { useState } from 'react';
 
 function App() {
-	const data = [
-		{
-			title: 'Подготовка', 
-			text: 'Горные походы',
-			date: new Date()
-		},
-		{
-			title: 'Подготовка2', 
-			text: 'Горные походы2',
-			date: new Date()
+	const INITIAL_DATA = [
+	 	// {
+		// 	id:1,
+	 	// 	title: 'Подготовка', 
+	 	// 	text: 'Горные походы',
+	 	// 	date: new Date()
+	 	// },
+	 	// {
+		// 	id:2,
+	 	// 	title: 'Подготовка2', 
+	 	// 	text: 'Горные походы2',
+	 	// 	date: new Date()
+	 	// }
+	 ];
+
+	 const [items, setItems] = useState(INITIAL_DATA);
+
+	 const addItem = (item) => {
+		setItems(oldItems=> [...oldItems, {
+			post: item.post,
+			title: item.title,
+			date: new Date(item.date),
+			id: oldItems.length > 0 ? Math.max(...oldItems.map(i => i.id)) + 1 : 1
+		}] );
+	 };
+
+	 const sortItems = (a, b) => {
+		if (a.date < b.date) {
+			return 1;
+		} else {
+			return -1;
 		}
-	];
+	 };
+
+	 let list = <p>Записей пока нет, добавьте первую</p>;
+	 if (items.length > 0) {
+		list = items.sort(sortItems).map(el => (
+			<CardButton key={el.id}>
+				<JournalItem
+					title={el.title}
+					post={el.post}
+					date={el.date}
+				></JournalItem>
+			</CardButton>));
+	 }
 
 	return (
 		<div className='app'>
@@ -28,24 +62,11 @@ function App() {
 				<Header></Header>
 				<JournalAddButton></JournalAddButton>
 				<JournalList>
-					<CardButton>
-						<JournalItem
-							title={data[0].title}
-							text={data[0].text}
-							date={data[0].date}
-						></JournalItem>
-					</CardButton>
-					<CardButton>
-						<JournalItem
-							title={data[1].title}
-							text={data[1].text}
-							date={data[1].date}
-						></JournalItem>
-					</CardButton>
+					{list}
 				</JournalList>
 			</LeftPanel>
 			<Body>
-				<JournalForm></JournalForm>
+				<JournalForm onSubmit={addItem}></JournalForm>
 			</Body>
 		</div>
 	);
--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,27 +1,63 @@
-import './JournalForm.css';
-import { useState } from 'react';
+import styles from './JournalForm.module.css';
 import Button from '../Button/Button';
+import { useState } from 'react';
 
-function JournalForm() {
-	const [inputData, setInputData] = useState('');
-
-	const inputChange = (e) => {
-		setInputData(e.target.value);
-	};
+function JournalForm({onSubmit}) {
+	const [formValidState, setFormValidState] = useState({
+		title: true,
+		post: true,
+		date: true
+	});
 
 	const addJournalItem = (e) => {
 		e.preventDefault();
 		const formData = new FormData(e.target);
 		const formProps = Object.fromEntries(formData);
-		console.log(formProps);
+		let isFormValid = true;
+		if (!formProps.title?.trim().length) {
+			setFormValidState(state => ({...state, title: false}));
+			isFormValid = false;
+		} else {
+			setFormValidState(state => ({...state, title: true}));
+		}
+		if (!formProps.post?.trim().length) {
+			setFormValidState(state => ({...state, post: false}));
+			isFormValid = false;
+		} else {
+			setFormValidState(state => ({...state, post: true}));
+		}
+		if (!formProps.date) {
+			setFormValidState(state => ({...state, date: false}));
+			isFormValid = false;
+		} else {
+			setFormValidState(state => ({...state, date: true}));
+		}
+		if (!isFormValid) {
+			return;
+		}
+		onSubmit(formProps);
 	};
 
 	return (
-		<form className='journal-form' onSubmit={addJournalItem}>	
-			<input type='text' name='title'></input>		
-			<input type='date' name='date'/>
-			<input type='text' name='tag' value={inputData} onChange={inputChange}/>
-			<textarea name='post' id='' cols={30} rows={10}></textarea>
+		<form className={styles['journal-form']} onSubmit={addJournalItem}>	
+			<div>
+				<input type='text' name='title' className={`${styles['input']} ${formValidState.title ? '' : styles['invalid']}`}></input>		
+			</div>
+			<div className={styles['form-row']}>
+				<label htmlFor="date" className={styles['form-label']}>
+					<img src='calendar.svg' alt='Иконка календаря'></img>
+					<span>Дата</span>
+				</label>
+				<input type='date' name='date' id="date" className={`${styles['input']} ${formValidState.date ? '' : styles['invalid']}`}/>
+			</div>
+			<div className={styles['form-row']}>
+				<label htmlFor="tag" className={styles['form-label']}>
+					<img src='folder.svg' alt='Иконка папки'></img>
+					<span>Метки</span>
+				</label>
+				<input type='text' name='tag' id="tag" className={styles['input']}/>
+			</div>
+			<textarea name='post' id='' cols={30} rows={10} className={`${styles['input']} ${formValidState.post ? '' : styles['invalid']}`}></textarea>
 			<Button text={'Сохранить'}></Button>
 		</form>
 		
